0d6261f364cf52a443f98180e932f88530f13507
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-11-16 00:49:44 +0000

    Third Commit

--- backend/app.py
+++ backend/app.py
@@ -39,6 +39,6 @@ def gita():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local testing
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
 
